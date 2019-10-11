65bf7849ee17fed8a561507b712b8f5db6b291de
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-10-11 09:26:28 -0500

    warmup

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -96,3 +96,10 @@ function arrayLength(array) {
     return array.length
 }
 
+function quotient(num1, num2) {
+if(typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 / num2;
+} else {
+    return false;
+    }
+}
