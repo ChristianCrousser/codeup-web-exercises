d93a69d3a1f3d3ef1a8a4474f0daa1597ee7ce68
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-12-17 10:24:37 -0600

    some adjustments

--- js/personal-site.js
+++ js/personal-site.js
@@ -1,2 +1,19 @@
 "use strict";
 
+function myMove() {
+    var elem = document.getElementById("longPiece");
+    var elem2 = document.getElementById("cubePiece");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 67) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'vh';
+            elem.style.bottom = pos + 'vh';
+            elem2.style.top = pos + 'vh';
+            elem2.style.bottom = pos + 'vh';
+        }
+    }
+}
